1ae7977f010f5a88f23ce1047ad4bbc1a5374a21
Lamiaa78 <100521392+Lamiaa78@users.noreply.github.com>
2025-07-08 08:31:26 +0200

    correction

--- helm/install_nginx_controller.sh
+++ helm/install_nginx_controller.sh
@@ -1,14 +1,19 @@
-#NGINX_EIP=$(terraform output -raw nginx_eip)
+#!/bin/bash
 
+# Add the ingress-nginx repository
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 
-kubectl create namespace ingress-nginx
-helm install nginx-controller ingress-nginx/ingress-nginx \
-  --namespace ingress-nginx \
-  --set controller.service.type="LoadBalancer"
-  
+# Create namespace for ingress-nginx
+kubectl create namespace ingress-nginx --dry-run=client -o yaml | kubectl apply -f -
 
+# Install the nginx-ingress controller
+helm upgrade --install nginx-controller ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --set controller.service.type=LoadBalancer \
+  --set controller.publishService.enabled=true \
+  --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-dns-label-name"="devops-cicd"
+  
 echo "Waiting for the nginx-controller to be ready"
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s
 echo "nginx-controller is ready"
